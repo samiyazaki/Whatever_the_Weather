55576e44aecbcb12d4c25f69efe163609ab01c54
Seiya Miyazaki <smsmiyazaki@gmail.com>
2022-12-14 16:56:58 -0500

    Adding js variables

---
+++ script.js
@@ -0,0 +1,4 @@
+var searchHistoryList = $('#search-history-list');
+var searchCityInput = $('#search-city');
+var searchCityButton = $('#search-city-button');
+var clearHistoryButton = $('#clear-history');
