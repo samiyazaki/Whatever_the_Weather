05ce93ab738389e0ded9c2c4ae36ab544703296f
Seiya Miyazaki <smsmiyazaki@gmail.com>
2022-12-15 18:33:22 -0500

    Adding API key

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ var searchHistoryList = $('#search-history-list');
 var searchCityInput = $('#search-city');
 var searchCityButton = $('#search-city-button');
 var clearHistoryButton = $('#clear-history');
+
+var currentCity =$("#current-city");
+var currentTemp =$("#current-temp");
+var currentHumidity =$("#current-humidity");
+var currentWindSpeed =$("current-wind-speed");
+var UVindex = $("#uv-index");
+
+var weatherContent = $("#weather-content");
+
+var APIkey = "a0af21eae700b0b06e138c8932d7db83";
+
