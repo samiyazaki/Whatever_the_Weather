e3fa5b4df2d385186cb41d456d82e7ab8abc670b
Seiya Miyazaki <smsmiyazaki@gmail.com>
2022-12-17 09:03:14 -0500

    More scripting, adding functions for the 5 day forecast

--- script.js
+++ script.js
@@ -50,11 +50,71 @@ function currentConditionsRequest(searchValue) {
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(functio(response){
+    }).then(function(response){
         console.log(response);
         currentCity.text(response.name);
         currentCity.append("<small class ='text-muted' id='current-date'>");
         $("#current-date").text("(" + currentDate + ")");
-        currentCity.append("<img src='https://openweathermap.org/img/w/" +response.weather[0].icon +B)
-    })
+        currentCity.append("<img src='https://openweathermap.org/img/w/" + response.weather[0].icon + ".png' alt='" + response.weather[0].main + "' />")
+        currentTemp.text(response.main.temp);
+        currentTemp.append("&deg;F");
+        currentHumidity.text(response.main.humidity +"%");
+        currentWindSpeed.text(response.wind.speed + "MPH");
+
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
+
+        var countryCode = response.sys.country;
+        var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?&units=imperial&appid=" + APIkey +"&lat=" + lat + "&lon=" + lon;
+
+        $.ajax({
+            url: forecastURL,
+            method: "GET"
+        }).then(function(response){
+            console.log(response);
+            $('#five-day-forecast').empty();
+            for (var i = 1; i< response.list.length; i+=8) {
+                var forecastDateString = dayjs(response.list[i].dt_txt).format("L");
+                console.log(forecastDateString);
+
+                var forecastCol= $("<div class-'col-12 col-md-6 col-lg forecast-day mb-3'>");
+                var forecastCard = $("<div class='card'>");
+                var forecastCardBody=$("<div class='card-body'>");
+                var forecastDate = $("<h5 class ='card-title'>");
+                var forecastIcon = $("<img>");
+                var forecastTemp = $("<p class ='card-text mb-0'>");
+                var forecastHumidity = $("<p class ='card-text mb-0'>");
+
+                forecastIcon.attr("src", "https://openweathermap.org/img/w/" + response.list[i].weather[0].icon + ".png");
+                forecastIcon.attr("alt", response.list[i].weather[0].main);
+                forecastDate.text(forecastDateString);
+                forecastTemp.text(response.list[i].main.temp);
+                forecastTemp.prepend("temp: ");
+                forecastTemp.append("&deg;F");
+                forecastHumidity.text(response.list[i].main.humidity);
+                forecastHumidity.prepend("Humidity: ");
+                forecastHumidity.append("%");
+            }
+        });
+
+        });
+
+    };
+
+function searchHistory(searchValue) {
+    if (searchValue) {
+        if (cityList.indexOf(searchValue) === -1) {
+            cityList.push(searchValue);
+
+            listArray();
+            clearHistoryButton.removeClass("hide");
+            weatherContent.removeClass("hide");
+        } else {
+            var removeIndex = cityList.indexOf(searchValue);
+            cityList.splice(removeIndex, 1);
+            cityList.push(searchValue);
+
+            listArray();
+        }
+    }
 }
