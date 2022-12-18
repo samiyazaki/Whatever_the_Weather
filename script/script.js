ece815dd44b36768d68c3a6d4b8fe0aed5408a5c
Seiya Miyazaki <smsmiyazaki@gmail.com>
2022-12-18 14:01:23 -0500

    mofre commmentary

--- script/script.js
+++ script/script.js
@@ -44,29 +44,29 @@ function currentConditionsRequest(searchValue) { //Here's where we make an AJAX 
     "&units=imperial&appid=a0af21eae700b0b06e138c8932d7db83"; // Sets the measurements to "colonizer" instead of the metric system because I'm from America and we are contrarians.
 
   $.ajax({ //AJAX call; not to the inferior greek prince of Salamis, but an Asynchronous JavaScript And XML call. To send and receive data in multiple formats.
-    url: queryURL, //using the variable URL we created with my API key
-    method: "GET", 
-  }).then(function (response) {
-    console.log(response);
-    currentCity.text(response.name);
-    currentCity.append("<small class ='text-muted' id='current-date'>");
+    url: queryURL, //using the variable URL we created with my API key to get the data for the current date for the city input
+    method: "GET", //get the data from the API site
+  }).then(function (get) { // get is the argument used to pass the data from the api
+    console.log(get);
+    currentCity.text(get.name); // sets the returned name to text
+    currentCity.append("<small class ='text-muted' id='current-date'>"); // adds dayjs data to the form
     $("#current-date").text("(" + currentDate + ")");
     currentCity.append(
       "<img src='https://openweathermap.org/img/w/" +
-        response.weather[0].icon +
+      get.weather[0].icon + // gets icon from openweathermap
         ".png' alt='" +
-        response.weather[0].main +
+        get.weather[0].main + //gets alt text from openweathermap
         "' />"
     );
-    currentTemp.text(response.main.temp);
-    currentTemp.append(" " + "&deg; F");
-    currentHumidity.text(response.main.humidity + " %");
-    currentWindSpeed.text(response.wind.speed + " MPH");
+    currentTemp.text(get.main.temp ); // pulls current temperature data from openweathermap
+    currentTemp.append(" " + "&deg; F"); // Adds the degree symbol and F for farenheit because I used imperial data. Couldn't get the degree symbol to work in the previous call so I added an append.
+    currentHumidity.text(get.main.humidity + " %"); // gets the humidity and adds %
+    currentWindSpeed.text(get.wind.speed + " MPH"); // gets the wind speed and adds MPH
 
-    var lat = response.coord.lat;
-    var lon = response.coord.lon;
+    var lat = get.coord.lat; // gets lat and long data from OpenWeatherMap and creates coordinates 
+    var lon = get.coord.lon;
 
-    var forecastURL =
+    var forecastURL = //setting up another URL for an AJAX call, but including the lat and lon coordinates
       "https://api.openweathermap.org/data/2.5/forecast?&units=imperial&appid=a0af21eae700b0b06e138c8932d7db83" +
 
       "&lat=" +
@@ -74,25 +74,25 @@ function currentConditionsRequest(searchValue) { //Here's where we make an AJAX 
       "&lon=" +
       lon;
 
-    $.ajax({
+    $.ajax({ //Looking familiar?
       url: forecastURL,
       method: "GET",
-    }).then(function (response) {
-      console.log(response);
+    }).then(function (get) {
+      console.log(get);
       $("#five-day-forecast").empty();
-      for (var i = 1; i < response.list.length; i += 8) {
-        var forecastDateString = dayjs(response.list[i].dt_txt).format(
-          "dddd, MM/DD/YYYY"
+      for (var i = 1; i < get.list.length; i += 8) { //Setting up the 5 day forecast data
+        var forecastDateString = dayjs(get.list[i].dt_txt).format(
+          "dddd, MM/DD/YYYY" // Using dayjs format to set the date info 
         );
         console.log(forecastDateString);
 
         var forecastCol = $(
-          "<div class='col-12 col-md-6 col-lg forecast-day mb-3'>"
+          "<div class='col-12 col-md-6 col-lg forecast-day mb-3'>" //creating the variables as cards so they can be created and populated with the data pulled from OpenWeatherMap
         );
         var forecastCard = $("<div class='card'>");
         var forecastCardBody = $("<div class='card-body'>");
-        var forecastDate = $("<h5 class ='card-title'>");
-        var forecastIcon = $("<img>");
+        var forecastDate = $("<h5 class ='card-title'>"); // giving the date a header 5 element to make it slightly bigger
+        var forecastIcon = $("<img>"); //including the pulled icon image
         var forecastTemp = $("<p class ='card-text mb-0'>");
         var forecastHumidity = $("<p class ='card-text mb-0'>");
         var forecastWindSpeed = $("<p class = 'card-text mb-0'>");
@@ -110,18 +110,18 @@ function currentConditionsRequest(searchValue) { //Here's where we make an AJAX 
         forecastIcon.attr(
           "src",
           "https://openweathermap.org/img/w/" +
-            response.list[i].weather[0].icon +
+          get.list[i].weather[0].icon +
             ".png"
         );
-        forecastIcon.attr("alt", response.list[i].weather[0].main);
+        forecastIcon.attr("alt", get.list[i].weather[0].main);
         forecastDate.text(forecastDateString);
-        forecastTemp.text(response.list[i].main.temp);
+        forecastTemp.text(get.list[i].main.temp);
         forecastTemp.prepend("Temp: ");
         forecastTemp.append(" &deg;F");
-        forecastHumidity.text(response.list[i].main.humidity);
+        forecastHumidity.text(get.list[i].main.humidity);
         forecastHumidity.prepend("Humidity: ");
         forecastHumidity.append(" %");
-        forecastWindSpeed.text(response.list[i].wind.speed);
+        forecastWindSpeed.text(get.list[i].wind.speed);
         forecastWindSpeed.prepend("Wind Speed: ");
         forecastWindSpeed.append("MPH");
       }
